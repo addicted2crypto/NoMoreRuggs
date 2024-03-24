f9110cd8302236e7695ff3232da74bb3da765933
William(addicted2crypto) <addicted2krypto@gmail.com>
2024-03-23 23:32:03 -0700

    testing rugg token deployment to testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  networks: {
   goerli: {
     url: process.env.ALCHEMY_GOERLI_ENDPOINT,
     accounts: [process.env.PRIVATE_KEY]
@@ -13,6 +14,11 @@ module.exports = {
     url: process.env.ALCHEMY_MAINNET_ENDPOINT,
     accounts: [process.env.PRIVATE_KEY]
   }
+},
 
   //add sepolia and avax here
+  etherscan: {
+    //Api key from https://etherscan.io/apikey
+    apiKey: process.env.ETHERSCAN_API_KEY
+  }
 };
