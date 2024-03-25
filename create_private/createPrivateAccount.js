09bb86c4b8e291f7a610d5df3fc050425735abe3
William(addicted2crypto) <addicted2krypto@gmail.com>
2024-03-24 20:36:01 -0700

    create private seed with web3 js library

--- create_private/createPrivateAccount.js
+++ create_private/createPrivateAccount.js
@@ -1,7 +1,7 @@
 // web3 library helps with not needing a wallet provider
 
-var Web3 = require("web3");
-const web3 = new Web3(Web3.givenProvider || "ws//localhost:8545");
+const {Web3} = require("web3");
+const web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
 
 async function main() {
     const account = web3.eth.accounts.create(web3.utils.randomHex(32));
