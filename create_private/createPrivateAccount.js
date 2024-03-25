c4e54329ca738d794613d009202657bde85e654d
William(addicted2crypto) <addicted2krypto@gmail.com>
2024-03-25 15:41:18 -0700

    msg

--- create_private/createPrivateAccount.js
+++ create_private/createPrivateAccount.js
@@ -1,6 +1,7 @@
 // web3 library helps with not needing a wallet provider
 
 const {Web3} = require("web3");
+//runs on server
 const web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
 
 async function main() {
