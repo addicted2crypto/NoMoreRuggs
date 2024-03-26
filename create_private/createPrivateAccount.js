1d63a0be0cc25607d60797fe748d2dad087001b1
William(addicted2crypto) <addicted2krypto@gmail.com>
2024-03-26 16:56:10 -0700

    log the first address

--- create_private/createPrivateAccount.js
+++ create_private/createPrivateAccount.js
@@ -7,6 +7,7 @@ const web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
 async function main() {
     const account = web3.eth.accounts.create(web3.utils.randomHex(32));
     console.log(account);
+    console.log(account[0])
 }
 
 
