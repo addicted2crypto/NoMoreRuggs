442d7ff9ccd1ae4a8b79f54e40d67bc5a046315d
William(addicted2crypto) <addicted2krypto@gmail.com>
2024-04-04 16:02:57 -0700

    constructor args

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,26 +1,30 @@
 const hre = require("hardhat");
+const { Contract } = require('hardhat/internal/hardhat-network/stack-traces/model');
 
 async function main() {
-
+    constructorArguments = [
+        "0xd955437119a0675F8A706C553E3e850386a91376",
+    ]
     const [deployer] = await ethers.getSigners();
 
     console.log("Deploying contract with account:" , deployer.address);
     
     
     //this factory lets us deploy
-    const NoMoreRuggs = await hre.ethers.getContractFactory("NoMoreRuggs");
+    const NoMoreRuggs = await ethers.getContractFactory("NoMoreRuggs");
     //this will deploy with 42 billy tokens
     const token = await NoMoreRuggs.deploy(4200000000000);
+    const contract = await NoMoreRuggs.deploy(...constructorArguments);
 
-    await token.waitForDeployment();
+    // await contract.deployTransaction.wait(5);
+    // await token.waitForDeployment(); depreciated??
 
-    console.log("Token address:", token.getAddress);
-    console.log("Account balances:" ,(await token.getAddress()).toString());
+    console.log(`Token address:" ${contract.address}`);
+    // console.log("Account balances:" ,(await token.getAddress()).toString());
 
-    await hre.run("verify:verify", {
+    run("verify:verify", {
         address: token.address,
         constructorArguments: [4200000000000]
-
     });
 }
 
